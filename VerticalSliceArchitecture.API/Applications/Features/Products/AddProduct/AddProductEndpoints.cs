ad0c2eb696f4aa6df2844baa5ca392e8414f892b
themuhsinoglu <muhsinogluahmet@gmail.com>
2024-10-21 18:22:31 +0300

    bug fluent

--- VerticalSliceArchitecture.API/Applications/Features/Products/AddProduct/AddProductEndpoints.cs
+++ VerticalSliceArchitecture.API/Applications/Features/Products/AddProduct/AddProductEndpoints.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using FluentValidation;
 
 namespace VerticalSliceArchitecture.API.Applications.Features.Products.AddProduct;
 
@@ -6,8 +7,16 @@ public static class AddProductEndpoints
 {
     public static void MapAddProductEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapPost("/api/products", async (IMediator mediator, AddProductCommand command) =>
+        app.MapPost("/api/products", async (IMediator mediator, AddProductCommand command, IValidator<AddProductCommand> validator) =>
             {
+
+                var validationResult = await validator.ValidateAsync(command);
+
+                if (!validationResult.IsValid)
+                {
+                    return Results.ValidationProblem(validationResult.ToDictionary());
+                }
+
                 var productId = await mediator.Send(command);
                 return Results.Ok(productId);
             })
